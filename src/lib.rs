41d172554588fa917be5ebb6dd26633b2cdb4923
higumachan <harekumo792154@gmail.com>
2022-08-30 12:42:26 +0900

    fullTextAnnotationを読めるようにする

--- src/lib.rs
+++ src/lib.rs
@@ -39,15 +39,105 @@ pub struct TextAnnotation {
     pub bounding_poly: Polygon,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct FullTextAnnotation {
+    pages: Vec<Page>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Page {
+    blocks: Vec<Block>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Block {
+    #[serde(rename = "blockType")]
+    block_type: String,
+    #[serde(rename = "boundingBox")]
+    bounding_box: BoundingBox,
+    confidence: f64,
+    paragraphs: Vec<Paragraph>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Paragraph {
+    #[serde(rename = "boundingBox")]
+    bounding_box: BoundingBox,
+    confidence: f64,
+    words: Vec<Word>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Word {
+    #[serde(rename = "boundingBox")]
+    bounding_box: BoundingBox,
+    confidence: f64,
+    #[serde(rename = "symbols")]
+    symbols: Vec<Symbol>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Symbol {
+    #[serde(rename = "boundingBox")]
+    bounding_box: BoundingBox,
+    confidence: f64,
+    text: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Polygon {
     pub vertices: Vec<Point>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 pub struct Point {
-    pub x: Option<i64>,
-    pub y: Option<i64>,
+    pub x: i64,
+    pub y: i64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BoundingBox {
+    vertices: Vec<Point>,
+}
+
+impl BoundingBox {
+    pub fn left_top(&self) -> Point {
+        self.vertices[1]
+    }
+    pub fn width(&self) -> i64 {
+        &self.vertices[3].x - &self.vertices[1].x
+    }
+
+    pub fn height(&self) -> i64 {
+        &self.vertices[3].y - &self.vertices[1].y
+    }
+}
+
+pub struct Response {
+    response: Value,
+}
+
+impl Response {
+    pub fn text_annotations(&self) -> anyhow::Result<Vec<TextAnnotation>> {
+        let json_response = &self.response;
+        let text_annotations_value = &json_response["responses"][0]["textAnnotations"];
+
+        Ok(text_annotations_value
+            .as_array()
+            .with_context(|| format!("text_annotations must be array: {}", json_response))?
+            .iter()
+            .map(|x| {
+                serde_json::from_value(x.clone()).expect("textAnnotation json value parse error")
+            })
+            .collect())
+    }
+
+    pub fn full_text_annotations(&self) -> anyhow::Result<FullTextAnnotation> {
+        let json_response = &self.response;
+        let full_text_annotations_value = &json_response["responses"][0]["fullTextAnnotation"];
+
+        Ok(serde_json::from_value(full_text_annotations_value.clone())?)
+    }
 }
 
 /// Client for google cloud vision
@@ -71,7 +161,7 @@ impl Client {
         Some(Self::new(std::env::var("GCV_API_KEY").ok()?.as_str()))
     }
 
-    pub async fn text_annotations(&self, image: &ImageGCV) -> anyhow::Result<Vec<TextAnnotation>> {
+    pub async fn request(&self, image: &ImageGCV) -> anyhow::Result<Response> {
         let request = json!({
            "requests" : [
                {
@@ -102,16 +192,9 @@ impl Client {
             return Err(anyhow::anyhow!(err.to_string()));
         }
 
-        let text_annotations_value = &json_response["responses"][0]["textAnnotations"];
-
-        Ok(text_annotations_value
-            .as_array()
-            .with_context(|| format!("text_annotations must be array: {}", json_response))?
-            .iter()
-            .map(|x| {
-                serde_json::from_value(x.clone()).expect("textAnnotation json value parse error")
-            })
-            .collect())
+        Ok(Response {
+            response: json_response,
+        })
     }
 }
 
@@ -133,10 +216,13 @@ mod tests {
             .decode()
             .unwrap();
         let gcv_image = ImageGCV::from_image(&image).unwrap();
-        let result = client.text_annotations(&gcv_image).await;
-        assert!(result.is_ok());
-        assert_eq!(result.as_ref().unwrap().len(), 2);
-        assert_eq!(result.as_ref().unwrap()[0].description, "44097050");
+        let resp = client.request(&gcv_image).await;
+        assert!(resp.is_ok());
+
+        let result = resp.unwrap().text_annotations().unwrap();
+
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].description, "44097050");
     }
     #[test]
     fn encode_() {
@@ -180,7 +266,27 @@ mod tests {
         assert_eq!(text_annotation.description, "ENGINE");
         assert_eq!(text_annotation.locale, None);
         assert_eq!(text_annotation.bounding_poly.vertices.len(), 4);
-        assert_eq!(text_annotation.bounding_poly.vertices[0].x, Some(1222));
-        assert_eq!(text_annotation.bounding_poly.vertices[0].y, Some(1771));
+        assert_eq!(text_annotation.bounding_poly.vertices[0].x, 1222);
+        assert_eq!(text_annotation.bounding_poly.vertices[0].y, 1771);
+    }
+
+    #[tokio::test]
+    async fn full_text_annotation() {
+        let client = Client::new(
+            std::env::var("GCV_API_KEY")
+                .expect("please set GCV_API_KEY")
+                .as_str(),
+        );
+        let mut image = ImageReader::open("test/test.png")
+            .unwrap()
+            .decode()
+            .unwrap();
+        let gcv_image = ImageGCV::from_image(&image).unwrap();
+        let resp = client.request(&gcv_image).await;
+        assert!(resp.is_ok());
+
+        let result = resp.unwrap().full_text_annotations().unwrap();
+
+        dbg!(result);
     }
 }
